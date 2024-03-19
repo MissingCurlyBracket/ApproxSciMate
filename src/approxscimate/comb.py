2dfaf326d3ba7269c06c5d52472017308107e258
Lucian Negru <L.Negru@student.tudelft.nl>
2024-03-19 18:43:05 +0100

    Update comb()

--- src/approxscimate/comb.py
+++ src/approxscimate/comb.py
@@ -2,19 +2,19 @@ import scipy.special as special
 import math
 
 
-def comb(n, k, l=0):
+def comb(n, k, level=0):
     """
     Calculates the number of ways to choose k items from a set of n distinct items without
-    regard to the order of selection. Has different levels of accuracy based on value of l.
+    regard to the order of selection. Has different levels of accuracy based on value of level.
     (Check package documentation for more details)
     :param n: integer
     :param k: integer
-    :param l: integer
+    :param level: integer
     :return: integer
     """
-    if l==0:
+    if level == 0:
         return special.comb(n, k)
-    if l==1:
-        return (special.gamma(n+1)) / (special.gamma(k+1) * special.gamma(n-k+1))
-    if l==2:
-        return (n**k) / math.factorial(k)
+    if level == 1:
+        return (special.gamma(n + 1)) / (special.gamma(k + 1) * special.gamma(n - k + 1))
+    if level == 2:
+        return (n ** k) / math.factorial(k)
